b025dec4c69e76563c7f04758d510e9e9c69a499
Katherine Pachal <katepachal@gmail.com>
2022-04-25 09:26:20 -0700

    ready to switch to cms dilepton

--- inputs/hl-lhc/dilepton_atlas/convert_dilepton.py
+++ inputs/hl-lhc/dilepton_atlas/convert_dilepton.py
@@ -1,3 +1,5 @@
+## ATLAS
+
 import ROOT
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,7 +12,7 @@ xvals_obs = []
 yvals_obs = []
 for i in range(exp_limit.GetN()) :
     xvals_obs.append(1000*exp_limit.GetPointX(i)) # convert to GeV
-    yvals_obs.append(exp_limit.GetPointY(i))
+    yvals_obs.append(exp_limit.GetPointY(i)*1e-3) # convert to pb
 xvals_obs = np.array(xvals_obs)
 yvals_obs = np.array(yvals_obs)
 
@@ -32,7 +34,7 @@ for i in range(th_ee_dm1TeV.GetN()) :
         exit(1)
     y_tot = th_ee_dm1TeV.GetPointY(i) + th_mumu_dm1TeV.GetPointY(i)
     xvals_th.append(1000*x_ee) # convert to GeV
-    yvals_th.append(y_tot)
+    yvals_th.append(y_tot*1e-3) # convert to pb
 xvals_th = np.array(xvals_th)
 yvals_th = np.array(yvals_th)
 
@@ -47,3 +49,17 @@ plt.savefig('dilepton_hl-lhc_validation.pdf',bbox_inches='tight')
 
 # And save to file
 np.savez('hl-lhc-dilepton.npz',xvals_theory=xvals_th,yvals_theory=yvals_th,xvals_obs=xvals_obs,yvals_obs=yvals_obs)
+
+# And a combined root file that matches
+mygraph_obs = ROOT.TGraph()
+for ix, iy in zip(xvals_obs, yvals_obs) :
+    mygraph_obs.SetPoint(mygraph_obs.GetN(), ix, iy)
+mygraph_th = ROOT.TGraph()
+for ix, iy in zip(xvals_th,yvals_th) :
+    mygraph_th.SetPoint(mygraph_th.GetN(),ix,iy)
+outfile = ROOT.TFile.Open("hl-lhc-dilepton.root","RECREATE")
+outfile.cd()
+mygraph_obs.Write("expected_limit")
+mygraph_th.Write("theory")
+outfile.Close()
+
--- inputs/hl-lhc/dilepton_cms/convert_dilepton.py
+++ inputs/hl-lhc/dilepton_cms/convert_dilepton.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import statistics as stats
+import ROOT
+
+# Input: units = picobarns I think.
 
 points_full = {}
 with open('ZPrime_limitCard_HLLHC_Exp.txt') as f :
@@ -17,7 +20,38 @@ for xval in points_full.keys() :
     yval = stats.median(points_full[xval])
     yvals.append(yval*1928*1.076923)
 
-# Plot it
+# Theory curve: take one with no acceptance factor from Jared and convert to pb.
+# Will need to extract and convert. Use the 1 so interpretation matches ATLAS.
+infile_ee = ROOT.TFile.Open("DMCrossSectionGraphs_axial_ee.root","READ")
+th_ee_dm1TeV = infile_ee.Get("graphFidXsec_1p0000")
+infile_ee.Close()
+infile_mumu = ROOT.TFile.Open("DMCrossSectionGraphs_axial_mumu.root","READ")
+th_mumu_dm1TeV = infile_mumu.Get("graphFidXsec_1p0000")
+th_mumu_dm1TeV_fullxsec = infile_mumu.Get("graphXsec_1p0000")
+infile_mumu.Close()
+
+acceptance = ROOT.TGraph()
+th_ee_dm1TeV_fullxsec = ROOT.TGraph()
+th_tot_fullxsec = ROOT.TGraph()
+xvals_th = []
+yvals_th = []
+for point in range(th_ee_dm1TeV.GetN()) :
+    xval = th_ee_dm1TeV.GetPointX(point)
+    yval_ee = th_ee_dm1TeV.GetPointY(point)*1e-3 # convert to pb
+    th_mumu_full = th_mumu_dm1TeV_fullxsec.Eval(xval)*1e-3 # convert to pb
+    th_mumu_fiducial = th_mumu_dm1TeV.Eval(xval)*1e-3 # convert to pb
+    acc = th_mumu_fiducial/th_mumu_full
+    xval_gev = xval*1000. # convert to GeV
+    acceptance.SetPoint(point,xval_gev,acc)
+    xsec_full = th_mumu_full+(yval_ee/acc)
+    th_ee_dm1TeV_fullxsec.SetPoint(point,xval_gev,yval_ee/acc)
+    th_tot_fullxsec.SetPoint(point,xval_gev,xsec_full)
+    xvals_th.append(xval_gev)
+    yvals_th.append(xsec_full)
+xvals_th = np.array(xvals_th)
+yvals_th = np.array(yvals_th)
+
+# Plot to validate
 print(xvals)
 print(yvals)
 fig,ax=plt.subplots(1,1)
@@ -25,4 +59,20 @@ plt.xlim(1250, 8000)
 #plt.ylim(1e-7, 10)
 plt.yscale('log')
 plt.plot(xvals,yvals)
+plt.plot(xvals_th,yvals_th)
 plt.savefig('dilepton_cms_hl-lhc_validation.pdf',bbox_inches='tight')
+
+# And save to file
+np.savez('hl-lhc-dilepton-cms.npz',xvals_theory=xvals_th,yvals_theory=yvals_th,xvals_obs=xvals,yvals_obs=yvals)
+
+# Output root file
+mygraph_obs = ROOT.TGraph()
+for ix, iy in zip(xvals, yvals) :
+    mygraph_obs.SetPoint(mygraph_obs.GetN(),ix,iy) # To same units
+outfile = ROOT.TFile.Open("hl-lhc-dilepton-cms.root","RECREATE")
+outfile.cd()
+acceptance.Write("acceptance")
+th_ee_dm1TeV_fullxsec.Write("graphXsec_1p0000")
+th_tot_fullxsec.Write("total_xsec_nb")
+mygraph_obs.Write("expected_limit_nb")
+outfile.Close()
